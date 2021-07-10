e1e99d484f8b8d5efe2b020bd55e767076d52192
Wilson Santos <wilson.santos.dev87@gmail.com>
2021-07-10 16:10:02 +0200

    Added fish

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,5 @@
+fish_vi_key_bindings
+
+set -g theme_display_git_default_branch yes
+set -g theme_nerd_fonts yes
+set theme_color_scheme gruvbox
--- install.sh
+++ install.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
-which stow || (echo "stow is not installed. Installing..." && sudo apt install stow -y)
+
+function install_package() {
+    package_name=$1
+    (which ${package_name} > /dev/null && \
+        echo "${package_name} is already installed" ) \
+        || (echo "${package_name} is not installed. Installing..." \
+        && sudo apt install ${package_name} -y)
+}
+
+function install_fish() {
+    install_package fish
+    grep "^$USER" /etc/passwd | grep fish > /dev/null || chsh -s /usr/bin/fish
+    # install Fira Code nerd fonts
+    # set -g theme_nerd_fonts yes
+    stow fish
+
+    # Themes:
+    # 1) install oh-my-fish
+    # curl -L https://get.oh-my.fish | fish
+    # 2) install bobthefish
+    # omf install bobthefish
+    # fish_config colors
+}
+
+install_package stow
 
 stow tmux
 stow vim
+
+install_fish
