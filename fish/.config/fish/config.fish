37f76af16c4e8f3e563dc382e12cf0c6d7c7f84f
Wilson Santos <wilson.santos.dev87@gmail.com>
2021-08-01 19:14:57 +0200

    Fixed powerline font to work with FiraCode

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,6 @@
 fish_vi_key_bindings
 
 set -g theme_display_git_default_branch yes
-set -g theme_nerd_fonts yes
+set -g theme_nerd_fonts no
+set -g theme_powerline_fonts yes
 set theme_color_scheme gruvbox
