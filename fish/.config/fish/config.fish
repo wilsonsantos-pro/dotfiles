65675ddb47cc0dd3dde3f26205d901007386ac0a
Wilson Santos <wilson.santos.dev87@gmail.com>
2022-12-29 16:53:56 +0100

    Updated fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,3 +4,7 @@ set -g theme_display_git_default_branch yes
 set -g theme_nerd_fonts no
 set -g theme_powerline_fonts yes
 set theme_color_scheme gruvbox
+theme_gruvbox dark soft
+
+alias cddotfiles "cd ~/dotfiles/"
+alias cdcosmicpython "cd ~/github/python_architecture_patterns_book/"
