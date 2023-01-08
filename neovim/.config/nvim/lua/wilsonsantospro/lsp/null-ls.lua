c5aaf5d49d7990f53c635216ab9ce5219a7f0431
Wilson Santos <wilson.santos.dev87@gmail.com>
2023-01-08 21:56:54 +0100

    Format nvim config

--- neovim/.config/nvim/lua/wilsonsantospro/lsp/null-ls.lua
+++ neovim/.config/nvim/lua/wilsonsantospro/lsp/null-ls.lua
@@ -1,6 +1,6 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
 if not null_ls_status_ok then
-    print("Error: null-ls not installed")
+	print("Error: null-ls not installed")
 	return
 end
 
@@ -15,24 +15,25 @@ null_ls.setup({
 	sources = {
 		formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
 		formatting.black.with({ extra_args = { "--fast" } }),
-        formatting.isort,
+		formatting.isort,
 		formatting.stylua,
         formatting.rustfmt,
         diagnostics.pylint,
+		diagnostics.pylint,
 	},
-    -- you can reuse a shared lspconfig on_attach callback here
-    on_attach = function(client, bufnr)
-        if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-            vim.api.nvim_create_autocmd("BufWritePre", {
-                group = augroup,
-                buffer = bufnr,
-                callback = function()
-                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
-                    vim.lsp.buf.format({ bufnr = bufnr })
-                    -- vim.lsp.buf.formatting_sync()
-                end,
-            })
-        end
-    end,
+	-- you can reuse a shared lspconfig on_attach callback here
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					-- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+					vim.lsp.buf.format({ bufnr = bufnr })
+					-- vim.lsp.buf.formatting_sync()
+				end,
+			})
+		end
+	end,
 })
