0a0a7cae729d038ca19d7dafc00cd91ed16485c9
Wilson Santos <wilson.santos.dev87@gmail.com>
2023-01-08 18:25:06 +0100

    Fixed huion.sh; with Nvidia driver it's HEAD-X instead of HDMI-X

--- scripts/huion.sh
+++ scripts/huion.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 pad_id=$(xsetwacom list | awk 'match($0, /id: ([0-9]+).*PAD/, a) {print a[1]}')
-pen_stylus_id=$(xsetwacom list | awk 'match($0, /id: ([0-9]+).*STYLUS/, a) {print a[1]}')
+pen_stylus_id=$(xsetwacom list | awk 'match($0, /id: ([0-9]+).*STYLUS/, a) {print a[1]}' | tail -1)
+tablet_output=$(xrandr --listactivemonitors | awk '/HDMI/ {print $NF}')
 
 # /usr/share/X11/xorg.conf.d/70-huion.conf
 ## Huion drawing tablet
@@ -28,8 +29,11 @@ pen_stylus_id=$(xsetwacom list | awk 'match($0, /id: ([0-9]+).*STYLUS/, a) {prin
 # $ sudo apt install xinput-calibrator
 # * Get MinX MinY MaxX MaxY from:
 # $ xinput_calibrator --device $pen_stylus_id
+echo "Setting Area.."
 xsetwacom set $pen_stylus_id Area "-143 -217 58650 32729"
 
+echo "Setting Buttons.."
 xsetwacom set $pad_id "Button" "12" "key ctrl z"
 
-xsetwacom set $pen_stylus_id MapToOutput HDMI-1
+echo "Mapping to output.."
+xsetwacom set $pen_stylus_id MapToOutput $tablet_output | grep -q Unable || xsetwacom set $pen_stylus_id MapToOutput HEAD-1
