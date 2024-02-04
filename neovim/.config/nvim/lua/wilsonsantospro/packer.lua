f1725b850ab26a68bc2105f5ed1eaa63febbfd04
Wilson Santos <wilson.santos.dev87@gmail.com>
2024-02-04 10:46:18 +0100

    Added nvim-autopairs

--- neovim/.config/nvim/lua/wilsonsantospro/packer.lua
+++ neovim/.config/nvim/lua/wilsonsantospro/packer.lua
@@ -38,77 +38,85 @@ packer.init({
 	},
 })
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
 	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+	use("wbthomason/packer.nvim")
 
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
 		-- or                            , branch = '0.1.x',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-    use 'sainnhe/gruvbox-material'
-    use 'navarasu/onedark.nvim'
-    use {
-        'nvim-lualine/lualine.nvim',
-        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
-    }
+	use("sainnhe/gruvbox-material")
+	use("navarasu/onedark.nvim")
+	use({
+		"nvim-lualine/lualine.nvim",
+		requires = { "kyazdani42/nvim-web-devicons", opt = true },
+	})
 
-	use {
-		'nvim-treesitter/nvim-treesitter',
+	use({
+		"nvim-treesitter/nvim-treesitter",
 		run = function()
-			local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
 			ts_update()
 		end,
-	}
+	})
 
-	use { 'theprimeagen/harpoon' }
-    use { 'voldikss/vim-floaterm' }
-	use { 'tpope/vim-fugitive' }
-    use { 'tpope/vim-commentary' }
-    use { 'tpope/vim-surround' }
-    use { 'qpkorr/vim-bufkill' }
+	use({ "theprimeagen/harpoon" })
+	use({ "voldikss/vim-floaterm" })
+	use({ "tpope/vim-fugitive" })
+	use({ "tpope/vim-commentary" })
+	use({ "tpope/vim-surround" })
+	use({ "qpkorr/vim-bufkill" })
 
-    use { 'preservim/nerdtree' }
-    use { 'ryanoasis/vim-devicons' }
-    use { 'Xuyuanp/nerdtree-git-plugin' }
-    use { 'airblade/vim-gitgutter' }
+	use({ "preservim/nerdtree" })
+	use({ "ryanoasis/vim-devicons" })
+	use({ "Xuyuanp/nerdtree-git-plugin" })
+	use({ "airblade/vim-gitgutter" })
 
-    -- Extra syntax
-    use { 'sophacles/vim-bundle-mako' }  -- mako templates syntax
-    use { 'pearofducks/ansible-vim' }   -- helps identify with filetype yaml.ansible
-    -- consider using: https://github.com/sheerun/vim-polyglot
+	-- Extra syntax
+	use({ "sophacles/vim-bundle-mako" }) -- mako templates syntax
+	use({ "pearofducks/ansible-vim" }) -- helps identify with filetype yaml.ansible
+	-- consider using: https://github.com/sheerun/vim-polyglot
 
-    use { 'AckslD/nvim-pytrize.lua' }
+	use({ "AckslD/nvim-pytrize.lua" })
 
-    use { 'kenn7/vim-arsync' } -- async remote file transfer
+	use({ "kenn7/vim-arsync" }) -- async remote file transfer
 
-    -- DAP
-    use { 'mfussenegger/nvim-dap' }
-    use { 'rcarriga/nvim-dap-ui', requires = {'mfussenegger/nvim-dap'} }
-    use { 'mfussenegger/nvim-dap-python' }
+	-- DAP
+	use({ "mfussenegger/nvim-dap" })
+	use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap" } })
+	use({ "mfussenegger/nvim-dap-python" })
 
-    -- COC
-    -- use {'neoclide/coc.nvim', branch = 'release'}
+	-- COC
+	-- use {'neoclide/coc.nvim', branch = 'release'}
 
-    -- Cmp
-    use "hrsh7th/nvim-cmp"
-    use "hrsh7th/cmp-buffer"
-    use "hrsh7th/cmp-path"
-	use "saadparwaiz1/cmp_luasnip"
-	use "hrsh7th/cmp-nvim-lsp"
-	use "hrsh7th/cmp-nvim-lua"
+	-- Cmp
+	use("hrsh7th/nvim-cmp")
+	use("hrsh7th/cmp-buffer")
+	use("hrsh7th/cmp-path")
+	use("saadparwaiz1/cmp_luasnip")
+	use("hrsh7th/cmp-nvim-lsp")
+	use("hrsh7th/cmp-nvim-lua")
 
 	-- Snippets
-    use "L3MON4D3/LuaSnip"
-    use "rafamadriz/friendly-snippets"
+	use("L3MON4D3/LuaSnip")
+	use("rafamadriz/friendly-snippets")
 
-    -- LSP
-	use "neovim/nvim-lspconfig"
-    use "williamboman/mason.nvim"
-    use "williamboman/mason-lspconfig.nvim"
-	use "jose-elias-alvarez/null-ls.nvim"
-    use "RRethy/vim-illuminate"
-    use "folke/neodev.nvim"
+	-- LSP
+	use("neovim/nvim-lspconfig")
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("RRethy/vim-illuminate")
+	use("folke/neodev.nvim")
+
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
 end)
