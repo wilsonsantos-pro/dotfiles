acca550e906c26e20b118d4a7fe00f04b20dd4a5
Wilson Santos <wilson.santos.dev87@gmail.com>
2025-07-26 11:26:26 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ function install_fish() {
     curl -L https://get.oh-my.fish | fish
 
     # Themes:
+    # https://github.com/rkbk60/onedark-fish
     fisher install rkbk60/onedark-fish
     # 1) install oh-my-fish
     # curl -L https://get.oh-my.fish | fish
@@ -39,6 +40,11 @@ function install_kitty() {
 }
 
 function install_vim() {
+    # install node, COC needs it
+    install_package nodejs
+    node --version
+
+    install_package vim
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
         https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
@@ -50,18 +56,26 @@ function install_nvim() {
     git clone --depth 1 https://github.com/wbthomason/packer.nvim \
         ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
-    # install ansible
-    python3 -m pip install -U --user ansible
-
     pushd ./ansible/
     ansible-playbook -K playbooks/install-neovim-locally-playbook.yml
     popd
 
     install_package ripgrep
+    install_package npm
+
+    nvim +PackerInstall +qa
+}
+
+function install_pipx () {
+    # install_package python3-pip
+    # python3 -m pip install --user pipx
+    install_package pipx
+    python3 -m pipx ensurepath
 }
 
 function install_ansible () {
-    pip install ansible-lint
+    pipx install ansible-lint
+    pipx install --include-deps ansible
 }
 
 function install_tmux () {
@@ -74,6 +88,7 @@ function install_tmux () {
 ## Start installation
 
 install_package stow
+install_package curl
 
 stow kitty
 stow neovim
@@ -81,12 +96,46 @@ stow tmux
 stow vifm
 stow vim
 
-install_fish
-install_kitty
+install_pipx
+install_ansible
+#install_fish
+#install_kitty
 install_vim
 install_nvim
 install_tmux
 
 # caps lock -> escape
-sudo apt install dconf-cli dconf-editor
+install_package dconf-cli
+install_package dconf-editor
 dconf write /org/gnome/desktop/input-sources/xkb-options "['caps:escape']"
+
+sudo apt install keepassx \
+    vlc \
+    diodon \
+    kupfer \
+    xfce4-terminal \
+    cpufrequtils \
+    htop \
+    gnome-tweaks \
+    -y
+
+install_package gnome-browser-connector
+
+# gnome-extensions
+# https://extensions.gnome.org/extension/1460/vitals/
+
+# gnome terminal one dark theme
+# https://github.com/denysdovhan/one-gnome-terminal
+bash -c "$(curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh)"
+
+#https://www.hjdskes.nl/projects/gcolor3/
+snap install gcolor3
+
+# always show the universal access menu (useful when using tablet mode to enable onscreen keyboard)
+gsettings set org.gnome.desktop.a11y always-show-universal-access-status true
+
+# Touchscreen in Firefox
+# https://askubuntu.com/questions/1122332/one-finger-scrolling-touchscreen-in-firefox
+# * open about:config in firefox to set dom.w3c_touch_events.enabled=1 (default is 2).
+# * edit /etc/security/pam_env.conf and add MOZ_USE_XINPUT2 DEFAULT=1
+# * reboot and restart firefox
